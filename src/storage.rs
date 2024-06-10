b79a95431fda442f2f0c9b3d8fee7589dd62920d
kindywu <kindywu@outlook.com>
2024-06-11 04:43:55 +0800

    add enum_dispatch

--- src/storage.rs
+++ src/storage.rs
@@ -6,7 +6,9 @@ pub use redb::*;
 
 use crate::{error::KvError, Kvpair, Value};
 use anyhow::Result;
+use enum_dispatch::enum_dispatch;
 
+#[enum_dispatch]
 pub trait Storage: Send + Sync {
     /// 从一个 HashTable 里获取一个 key 的 value
     fn get(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
@@ -19,4 +21,10 @@ pub trait Storage: Send + Sync {
 
     /// 遍历 HashTable，返回 kv pair 的 Iterator
     fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError>;
+}
+
+#[enum_dispatch(Storage)]
+pub enum StorageState {
+    MemoryStorage(MemoryStorage),
+    RedbStorage(RedbStorage),
 }
