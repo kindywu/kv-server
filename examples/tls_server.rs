7250a2364b62eccd84346faa137d37d68d13ef43
kindywu <kindywu@outlook.com>
2024-06-15 17:47:21 +0800

    add tls example

---
+++ examples/tls_server.rs
@@ -0,0 +1,120 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader};
+use std::net::ToSocketAddrs;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+
+use argh::FromArgs;
+use pki_types::{CertificateDer, PrivateKeyDer};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use tokio::io::{copy, sink, split, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio_rustls::rustls::pki_types;
+use tokio_rustls::{rustls, TlsAcceptor};
+
+/// Tokio Rustls server example
+#[derive(FromArgs)]
+struct Options {
+    /// bind addr
+    #[argh(positional)]
+    addr: String,
+
+    /// cert file
+    #[argh(option, short = 'c')]
+    cert: PathBuf,
+
+    /// key file
+    #[argh(option, short = 'k')]
+    key: PathBuf,
+
+    /// echo mode
+    #[argh(switch, short = 'e')]
+    echo_mode: bool,
+}
+
+fn load_certs(path: &Path) -> io::Result<Vec<CertificateDer<'static>>> {
+    // println!("load_certs {path:?}");
+    certs(&mut BufReader::new(File::open(path)?)).collect()
+}
+
+fn load_keys(path: &Path) -> io::Result<PrivateKeyDer<'static>> {
+    // println!("load_keys {path:?}");
+
+    pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
+        .next()
+        .unwrap()
+        .map(Into::into)
+}
+
+// cargo run --example tls_server -- 127.0.0.1:8080 --cert fixtures/server.cert --key fixtures/server.key
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    match env::current_dir() {
+        Ok(path) => println!("current dir: {}", path.display()),
+        Err(e) => println!("try to get current dir with error: {}", e),
+    }
+
+    let options: Options = argh::from_env();
+
+    let addr = options
+        .addr
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| io::Error::from(io::ErrorKind::AddrNotAvailable))?;
+    let certs = load_certs(&options.cert)?;
+    // println!("{certs:?}");
+    let key = load_keys(&options.key)?;
+    // println!("{key:?}");
+    let flag_echo = options.echo_mode;
+
+    let config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind(&addr).await?;
+
+    println!("listen on {addr}");
+
+    loop {
+        let (stream, peer_addr) = listener.accept().await?;
+        println!("peer_addr {peer_addr}");
+        let acceptor = acceptor.clone();
+
+        let fut = async move {
+            let mut stream = acceptor.accept(stream).await?;
+
+            if flag_echo {
+                let (mut reader, mut writer) = split(stream);
+                let n = copy(&mut reader, &mut writer).await?;
+                writer.flush().await?;
+                println!("Echo: {} - {}", peer_addr, n);
+            } else {
+                let mut output = sink();
+                stream
+                    .write_all(
+                        &b"HTTP/1.0 200 ok\r\n\
+                    Connection: close\r\n\
+                    Content-length: 12\r\n\
+                    \r\n\
+                    Hello world!"[..],
+                    )
+                    .await?;
+                stream.shutdown().await?;
+                copy(&mut stream, &mut output).await?;
+                println!("Hello: {}", peer_addr);
+            }
+
+            Ok(()) as io::Result<()>
+        };
+
+        tokio::spawn(async move {
+            if let Err(err) = fut.await {
+                eprintln!("{:?}", err);
+            }
+        });
+    }
+}
